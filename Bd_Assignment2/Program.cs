23c3a0a936981e1635312c7f550b8beb910fa3f5
mnhoque <naz05haq@gmail.com>
2020-10-13 08:54:22 +0200

    Add project files.

---
+++ Bd_Assignment2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Bd_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class DataOperation<T> where T : class
+        {
+            public void Insert(T item)
+            {
+
+            }
+
+            public void Update(T item)
+            {
+
+            }
+
+            public void Delete(T item)
+            {
+
+            }
+
+            public void GetById(int id)
+            {
+
+            }
+
+            public void GetAll()
+            {
+
+            }
+
+        }
+    }
+}
